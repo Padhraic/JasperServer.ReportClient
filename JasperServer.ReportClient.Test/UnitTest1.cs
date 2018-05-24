673270585ccfdaaa7fbc86aea4a0ff3bca9aab02
Padhraic Coogan <pcoogan@gmail.com>
2018-05-24 23:04:26 +0900

    rename project files

---
+++ JasperServer.ReportClient.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JasperServer.ReportClient.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
